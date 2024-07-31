4dce57ee85f568cf8b0f0abb188a5a7040232c88
Malower <maciejzych2003@gmail.com>
2024-07-31 13:27:45 +0200

    Kosmetyka

--- src/routes/komponenty/ShowLogs.svelte
+++ src/routes/komponenty/ShowLogs.svelte
@@ -79,9 +79,9 @@
   <div class="date-filters mb-4">
     <h2 class="mb-4 text-lg font-semibold">Wybierz zakres dat aby wyświetlić logowania</h2>
     <ul class="flex space-x-2">
-      <li><button class="btn" on:click={() => { filterLogs("today") }}>Dzisiaj</button></li>
-      <li><button class="btn" on:click={() => { filterLogs("week") }}>Tydzień</button></li>
-      <li><button class="btn" on:click={() => { filterLogs("month") }}>Miesiąc</button></li>
+      <li><button class="btn" on:click={() =>  filterLogs("today")}>Dzisiaj</button></li>
+      <li><button class="btn" on:click={() => filterLogs("week")}>Tydzień</button></li>
+      <li><button class="btn" on:click={() => filterLogs("month")}>Miesiąc</button></li>
       <li><button class="btn" on:click={() => showCustomDateRange()}>Niestandardowy</button></li>
     </ul>
   </div>
