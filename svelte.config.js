5f2e6ced6d0371df3d9387b826b2a1751112c6c2
Malower <maciejzych2003@gmail.com>
2024-08-07 10:52:24 +0200

    CloudFlare Może zadziała

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,27 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		}),
-		alias: {
-			$db: './src/db',
-		}
-	}
+  kit: {
+    adapter: adapter({
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+      platformProxy: {
+        configPath: "wrangler.toml",
+        environment: undefined,
+        experimentalJsonConfig: false,
+        persist: false,
+      },
+    }),
+    alias: {
+      $db: "./src/db",
+    },
+  },
 };
 
 export default config;
